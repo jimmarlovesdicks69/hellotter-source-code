7a9a0fd022c7723efe77d298eee7ceedaf8b85a2
Rg <reygabriellepogado@gmail.com>
2020-07-07 18:13:16 +0800

    Fix android filters and background panel

--- components/BackgroundView.js
+++ components/BackgroundView.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import * as Animatable from 'react-native-animatable';
-import { Dimensions, View } from 'react-native';
+import { Dimensions, View, Platform } from 'react-native';
 import BackgroundPanel from './BackgroundPanel';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
@@ -10,17 +10,26 @@ const screenWidth = Math.round(Dimensions.get('window').width);
 const BackgroundView = (props) => {
 
     return (
-        <SafeAreaView style={{ backgroundColor: 'black' }}>
-            <SafeAreaView>
-                <Animatable.View duration={500} animation={props.showBackgroundPanel ? "fadeInUp" : "fadeOutDown"}
-                    style={{ height: screenHeight, position: 'absolute', right: 0, bottom: 0, width: screenWidth }} >
-                    <View style={{ flexGrow: 1, backgroundColor: 'transparent' }} onTouchStart={() => props.onBackdropPressed()} />
-                    <View style={{ backgroundColor: 'rgba(0,0,0,0.8)', width: screenWidth, }}>
-                        <BackgroundPanel />
-                    </View>
-                </Animatable.View>
-            </SafeAreaView>
-        </SafeAreaView>
+        Platform.OS == 'ios' ?
+            <SafeAreaView style={{ backgroundColor: 'black' }}>
+                <SafeAreaView>
+                    <Animatable.View duration={500} animation={props.showBackgroundPanel ? "fadeInUp" : "fadeOutDown"}
+                        style={{ height: screenHeight, position: 'absolute', right: 0, bottom: 0, width: screenWidth }} >
+                        <View style={{ flexGrow: 1, backgroundColor: 'transparent' }} onTouchStart={() => props.onBackdropPressed()} />
+                        <View style={{ backgroundColor: 'rgba(0,0,0,0.8)', width: screenWidth, }}>
+                            <BackgroundPanel />
+                        </View>
+                    </Animatable.View>
+                </SafeAreaView>
+            </SafeAreaView> :
+            <Animatable.View duration={500} animation={props.showBackgroundPanel ? "fadeInUp" : "fadeOutDown"}
+                style={{ height: screenHeight, position: 'absolute', right: 0, bottom: 0, width: screenWidth }} >
+                <View style={{ flexGrow: 1, backgroundColor: 'transparent' }} onTouchStart={() => props.onBackdropPressed()} />
+                <View style={{ backgroundColor: 'rgba(0,0,0,0.8)', width: screenWidth, }}>
+                    <BackgroundPanel />
+                </View>
+            </Animatable.View>
+
     );
 };
 
